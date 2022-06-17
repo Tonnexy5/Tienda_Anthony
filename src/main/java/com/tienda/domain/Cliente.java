d727322528aa7f9545afa94a1c5bed187a47d441
antho <antho@TonnexyAsusTUF>
2022-06-17 09:52:35 -0600

    Semana 4 v2

--- src/main/java/com/tienda/domain/Cliente.java
+++ src/main/java/com/tienda/domain/Cliente.java
@@ -1,9 +1,25 @@
 package com.tienda.domain;
 
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import lombok.Data;
 
 @Data
-public class Cliente {
+@Entity
+@Table(name="cliente")
+public class Cliente implements Serializable {
+    
+    //Para utilizarlo en la generación de la llave primaria autoincremental
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idCliente;
+            
     String nombre;
     String apellidos;
     String correo;
